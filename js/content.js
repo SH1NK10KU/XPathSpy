e8c0cd08668deefb59743e2724857636f09f6dee
Shin Feng <shin.f.kan@gmail.com>
2015-01-19 09:35:19 +0800

    Add exceptional handling.

--- js/content.js
+++ js/content.js
@@ -13,19 +13,23 @@ chrome.runtime.onMessage.addListener(function(request, sender) {
             theResult = "Please input valid XPath!";
             console.error(theResult);
         } else {
-            var elements = document.evaluate(request.xpath, document, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null);
-            elementCount = elements.snapshotLength;
-            if (elementCount > 0) {
-                theResult = "There " + (elementCount > 1 ? "are " + elementCount + " elements" : "is only one element") + " which XPath likes \'" + request.xpath + "\' in this page."
-                console.log(theResult);
-            } else {
-                theResult = "There is no such element in this page!";
-                console.error(theResult);
+            try {
+                var elements = document.evaluate(request.xpath, document, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null);
+                elementCount = elements.snapshotLength;
+                if (elementCount > 0) {
+                    theResult = "There " + (elementCount > 1 ? "are " + elementCount + " elements" : "is only one element") + " which XPath likes \'" + request.xpath + "\' in this page."
+                    console.log(theResult);
+                } else {
+                    theResult = "There is no such element in this page!";
+                    console.error(theResult);
+                }
+            } catch (e) {
+                theResult = e.message.substr(e.message.indexOf("The string"));
             }
-            chrome.runtime.sendMessage({
-                message: "xpathspy",
-                result: theResult
-            });
         }
+        chrome.runtime.sendMessage({
+            message: "xpathspy",
+            result: theResult
+        });
     }
 });
